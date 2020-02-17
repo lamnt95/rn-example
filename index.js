b73a82835ef505ddf554e5119e103e2a54396f33
Nguyen Tung Lam <tunglam20132208@gmail.com>
2020-02-17 22:39:58 +0700

    install dependencies react native navigation

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+function NavigationContainerWrapper() {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => NavigationContainerWrapper);
