e73ab12d7974607d0dd6c39e3a6d43ce51fea0a7
Nguyen Tung Lam <tunglam20132208@gmail.com>
2020-02-27 19:45:09 +0700

    add login && react native device info

--- src/store/ducks/auth.ts
+++ src/store/ducks/auth.ts
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import Immutable from 'seamless-immutable';
 import {switchMap} from 'rxjs/operators';
-import epic from '../common/epic';
 import {authenWithVnd} from '../services/auth';
 import {ofType} from 'redux-observable';
 
@@ -9,6 +8,7 @@ export const types = {
   LOGIN_START: 'AUTH/LOGIN_START',
   LOGIN_SUCCESS: 'AUTH/LOGIN_SUCCESS',
   LOGIN_FAIL: 'AUTH/LOGIN_FAIL',
+  ADD_DEVICE_ID: 'AUTH/ADD_DEVICE_ID',
 };
 
 export const actions = {
@@ -27,6 +27,11 @@ export const actions = {
     error,
     meta,
   }),
+  addDeviceId: (payload, meta) => ({
+    type: types.ADD_DEVICE_ID,
+    payload,
+    meta,
+  }),
 };
 
 export const selectors = {
@@ -34,7 +39,10 @@ export const selectors = {
   getDeviceId: state => _.get(state, 'auth.deviceId'),
 };
 
-export const initState = {};
+export const initState = {
+  accessToken: undefined,
+  deviceId: undefined,
+};
 
 export function reducer(state = initState, action) {
   const auth = _.get(action, 'payload.auth');
@@ -47,14 +55,14 @@ export function reducer(state = initState, action) {
 export const loginStartEpic = (action$, store) =>
   action$.pipe(
     ofType(types.LOGIN_START),
-    switchMap(async ({meta}) => {
+    switchMap(async ({payload, meta}) => {
       const state = store.value;
-      const accessToken = selectors.getAccessToken(state);
+      const accessToken = _.get(payload, 'auth.accessToken');
       const deviceId = selectors.getDeviceId(state);
       const {scope} = meta || {};
       try {
-        const res = await authenWithVnd(deviceId, accessToken);
-        return Promise.resolve(actions.loginSuccess({auth: res}, {scope}));
+        await authenWithVnd(deviceId, accessToken);
+        return Promise.resolve(actions.loginSuccess(null, {scope}));
       } catch (e) {
         return Promise.resolve(actions.loginFail(e, {scope}));
       }
--- src/ui/screens/Login.js
+++ src/ui/screens/Login.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import queryString from 'query-string';
+import _ from 'lodash';
+import {View, Text, Button, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
+import WebView from 'react-native-webview';
+import DeviceInfo from 'react-native-device-info';
 
 import {actions} from '../../store/ducks';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 function mapStateToProps(state) {
   return {
@@ -14,26 +27,91 @@ function mapDispatchToProps(dispatch) {
   return {dispatch};
 }
 
+function getParams(urlString) {
+  try {
+    const index = urlString.indexOf('?');
+    const offset = urlString.indexOf('#');
+    const end = offset > index ? offset : urlString.length;
+
+    const search = urlString.substring(index, end);
+    return queryString.parse(search);
+  } catch (error) {
+    return {};
+  }
+}
+
 class Login extends React.Component {
   constructor() {
     super();
-
+    this.state = {
+      isShowWebView: false,
+      isComplete: false,
+    };
     this.onPressLoginVND = this.onPressLoginVND.bind(this);
+    this.stopLoading = this.stopLoading.bind(this);
+    this.onWebViewStateChange = this.onWebViewStateChange.bind(this);
   }
 
   componentDidMount() {
     const {dispatch} = this.props;
-    dispatch(actions.loginStart());
+    const devideId = DeviceInfo.getDeviceId();
+    dispatch(
+      actions.addDeviceId({
+        auth: {
+          devideId,
+        },
+      }),
+    );
+  }
+
+  onWebViewStateChange(state) {
+    if (!this.state.isComplete) {
+      const {url} = state;
+      const params = getParams(url);
+      const accessToken = _.get(params, 'token-id');
+      if (accessToken) {
+        this.setState({
+          isShowWebView: false,
+        });
+        const {dispatch} = this.props;
+        dispatch(actions.loginStart({auth: {accessToken}}));
+        this.stopLoading();
+      }
+    }
   }
 
   onPressLoginVND() {
-    console.log('onPressLoginVND');
+    this.setState({
+      isShowWebView: true,
+    });
+  }
+
+  stopLoading() {
+    if (this.refWebview) {
+      this.refWebview.stopLoading();
+    }
+  }
+
+  renderWebView() {
+    return (
+      <WebView
+        ref={ref => {
+          this.refWebview = ref;
+        }}
+        source={{
+          uri:
+            'https://id-uat.vndirect.com.vn/logout?httpReferer=https://stockbook.vn/receive-token-vnd',
+        }}
+        onNavigationStateChange={this.onWebViewStateChange}
+      />
+    );
   }
 
   render() {
-    console.log(this.props);
+    const {isShowWebView} = this.state;
+    if (isShowWebView) return this.renderWebView();
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Login screen</Text>
         <Button onPress={this.onPressLoginVND} title="Login VND" />
       </View>
